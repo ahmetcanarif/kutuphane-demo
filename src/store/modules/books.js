63c7eda1d812f2e7ac1304b411791b5610a543e4
ahmetcanarif <ahmtcn19037@gmail.com>
2020-01-06 20:28:47 +0300

    book.js modified

--- src/store/modules/books.js
+++ src/store/modules/books.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const state = {
   books: [],
+  bookDetail: {},
   categories: [],
   popularBooks: [],
   lastBooks: []
@@ -9,7 +10,8 @@ const getters = {
   getBooks: state => state.books,
   getPopularBook: state => state.popularBooks,
   getLastBook: state => state.lastBooks,
-  getCategory: state => state.categories
+  getCategory: state => state.categories,
+  getBookDetail: state => state.bookDetail
 };
 const mutations = {
   SET_BOOKS(state, books) {
@@ -23,48 +25,79 @@ const mutations = {
   },
   SET_CATEGORIES(state, categories) {
     state.categories = categories;
+  },
+  SET_BOOK_DETAIL(state, book) {
+    state.bookDetail = book;
   }
 };
 const actions = {
   async getData({ commit }) {
-    await axios
-      .get("http://localhost/api/allBooks.php")
-      .then(res => {
-        commit("SET_BOOKS", res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    return await new Promise((resolve, reject) => {
+      axios
+        .get("http://localhost/api/allBooks.php")
+        .then(res => {
+          commit("SET_BOOKS", res.data.books);
+
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   },
   async getPopularBooks({ commit }) {
-    await axios
-      .get("http://localhost/api/popularBooks.php")
-      .then(res => {
-        commit("SET_POPULAR_BOOKS", res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    return await new Promise((resolve, reject) => {
+      axios
+        .get("http://localhost/api/popularBooks.php")
+        .then(res => {
+          commit("SET_POPULAR_BOOKS", res.data.popularBooks);
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   },
   async getLastBooks({ commit }) {
-    await axios
-      .get("http://localhost/api/lastBooks.php")
-      .then(res => {
-        commit("SET_LAST_BOOKS", res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    return await new Promise((resolve, reject) => {
+      axios
+        .get("http://localhost/api/lastBooks.php")
+        .then(res => {
+          commit("SET_LAST_BOOKS", res.data.lastBooks);
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   },
   async getCategory({ commit }) {
-    await axios
-      .get("http://localhost/api/categories.php")
-      .then(res => {
-        commit("SET_CATEGORIES", res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    return await new Promise((resolve, reject) => {
+      axios
+        .get("http://localhost/api/categories.php")
+        .then(res => {
+          commit("SET_CATEGORIES", res.data.category);
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  },
+  async getOneBook({ commit }, id) {
+    return await new Promise((resolve, reject) => {
+      axios
+        .post("http://localhost/api/bookDetail.php", {
+          id: id
+        })
+        .then(res => {
+          commit("SET_BOOK_DETAIL", res.data.book);
+          resolve(res.data);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
 };
 
